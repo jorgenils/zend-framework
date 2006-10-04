443d5b233e1ef9ffd76f25d4ceae05bfa5ac0aa3
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2006-10-04 20:12:09 +0000

    * Fixed notice in Zend_Server_Reflection_Function_Abstract * Added Zend_XmlRpc_Server_Fault and Zend_XmlRpc_Server_Cache tests
    git-svn-id: http://framework.zend.com/svn/framework@1172 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ branch/zend_server/tests/Zend/XmlRpc/Server/CacheTest.php
@@ -0,0 +1,88 @@
+<?php
+require_once 'Zend/XmlRpc/Server.php';
+require_once 'Zend/XmlRpc/Server/Cache.php';
+require_once 'PHPUnit2/Framework/TestCase.php';
+require_once 'PHPUnit2/Framework/IncompleteTestError.php';
+
+/**
+ * Test case for Zend_XmlRpc_Server_Cache
+ *
+ * @package ortus
+ * @version $Id$
+ */
+class Zend_XmlRpc_Server_CacheTest extends PHPUnit2_Framework_TestCase 
+{
+    /**
+     * Zend_XmlRpc_Server object
+     * @var Zend_XmlRpc_Server
+     */
+    protected $_server;
+
+    /**
+     * Description
+     * @var string 
+     * @access private
+     */
+    protected $_file;
+
+    /**
+     * Setup environment
+     */
+    public function setUp() 
+    {
+        $this->_file = realpath(dirname(__FILE__)) . '/xmlrpc.cache';
+        $this->_server = new Zend_XmlRpc_Server();
+        $this->_server->setClass('Zend_XmlRpc_Server_CacheTest', 'cache');
+    }
+
+    /**
+     * Teardown environment
+     */
+    public function tearDown() 
+    {
+        if (file_exists($this->_file)) {
+            unlink($this->_file);
+        }
+        unset($this->_server);
+    }
+
+    /**
+     * Tests functionality of both get() and save()
+     */
+    public function testGetSave()
+    {
+        // Remove this line once the test has been written
+        if (!is_writeable('./')) {
+            throw new PHPUnit2_Framework_IncompleteTestError('Directory not writeable');
+        }
+
+        $this->assertTrue(Zend_XmlRpc_Server_Cache::save($this->_file, $this->_server));
+        $expected = $this->_server->listMethods();
+        $server = new Zend_XmlRpc_Server();
+        $this->assertTrue(Zend_XmlRpc_Server_Cache::get($this->_file, $server));
+        $actual = $server->listMethods();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Zend_XmlRpc_Server_Cache::delete() test
+     *
+     * Call statically 
+     *
+     * Expects:
+     * - filename: 
+     * 
+     * Returns: boolean 
+     */
+    public function testDelete()
+    {
+        // Remove this line once the test has been written
+        if (!is_writeable('./')) {
+            throw new PHPUnit2_Framework_IncompleteTestError('Directory not writeable');
+        }
+
+        $this->assertTrue(Zend_XmlRpc_Server_Cache::save($this->_file, $this->_server));
+        $this->assertTrue(Zend_XmlRpc_Server_Cache::delete($this->_file));
+    }
+}
