051b40448cb55bfa4209ec5308827d73b08a9e47
bkarwin <bkarwin@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2007-06-08 21:02:06 +0000

    Tag for ZF 1.0.0 RC2
    git-svn-id: http://framework.zend.com/svn/framework@5194 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ tag/release-1.0.0RC2/tests/Zend/Controller/Action/Helper/FlashMessengerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+require_once 'Zend/Controller/Front.php';
+require_once 'Zend/Controller/Request/Http.php';
+require_once 'Zend/Controller/Response/Cli.php';
+
+require_once 'Zend/Controller/Action/HelperBroker.php';
+
+require_once 'Zend/Session.php';
+
+class Zend_Controller_Action_Helper_FlashMessengerTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $savePath = ini_get('session.save_path');
+        if (strpos($savePath, ';')) {
+            $savePath = explode(';', $savePath);
+            $savePath = array_pop($savePath);
+        }
+        if (empty($savePath)) {
+            $this->markTestSkipped('Cannot test FlashMessenger due to unavailable session save path');
+        }
+
+        if (headers_sent()) {
+            $this->markTestSkipped('Cannot test FlashMessenger: cannot start session because headers already sent');
+        }
+        Zend_Session::start();
+    }
+       
+    public function testLoadFlashMessenger()
+    {
+        $controller = Zend_Controller_Front::getInstance();
+        $controller->resetInstance();
+        $controller->setControllerDirectory(dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . '_files');
+        $request = new Zend_Controller_Request_Http('http://framework.zend.com/helper-flash-messenger/');
+        $controller->setResponse(new Zend_Controller_Response_Cli());
+        
+        $controller->returnResponse(true);
+        $response = $controller->dispatch($request);
+        $this->assertEquals('Zend_Controller_Action_Helper_FlashMessenger123456', $response->getBody());
+    }
+}
