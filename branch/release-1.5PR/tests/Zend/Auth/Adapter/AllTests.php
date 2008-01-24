e9234d25fc80da195ec5b4981b8dba04c6087335
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-01-24 17:14:39 +0000

    * Comment out Zend_Auth_Adapter_OpenId tests, as they cause fatal error
    git-svn-id: http://framework.zend.com/svn/framework@7611 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- branch/release-1.5PR/tests/Zend/Auth/Adapter/AllTests.php
+++ branch/release-1.5PR/tests/Zend/Auth/Adapter/AllTests.php
@@ -55,7 +55,7 @@ require_once 'Zend/Auth/Adapter/Ldap/AllTests.php';
 /**
  * @see Zend_Auth_Adapter_OpenId_AllTests
  */
-require_once 'Zend/Auth/Adapter/OpenId/AllTests.php';
+//require_once 'Zend/Auth/Adapter/OpenId/AllTests.php';
 
 
 /**
@@ -90,7 +90,7 @@ class Zend_Auth_Adapter_AllTests
         $suite->addTestSuite('Zend_Auth_Adapter_DigestTest');
         $suite->addTest(Zend_Auth_Adapter_Http_AllTests::suite());
         $suite->addTest(Zend_Auth_Adapter_Ldap_AllTests::suite());
-        $suite->addTest(Zend_Auth_Adapter_OpenId_AllTests::suite());
+        //$suite->addTest(Zend_Auth_Adapter_OpenId_AllTests::suite());
 
         return $suite;
     }
