6fcda93bb7d7ae75a66ba44e772969df00c82507
darby <darby@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-01-28 17:19:18 +0000

    * Removing release-1.5.0PR tag that includes build-tools/, incubator/, and scripts/; will re-tag shortly
    git-svn-id: http://framework.zend.com/svn/framework@7660 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tag/release-1.5.0PR/incubator/library/Zend/Build/Profile/basic/htdocs/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'Zend/Controller/Front.php';
-Zend_Controller_Front::run('../application/controllers');
-
--- tag/release-1.5.0PR/library/Zend/Service/SlideShare/Exception.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once 'Zend/Exception.php';
-
-class Zend_Service_SlideShare_Exception extends Zend_Exception { }
