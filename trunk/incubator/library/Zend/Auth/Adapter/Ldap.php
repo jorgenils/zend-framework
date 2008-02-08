jorgenils/zend-framework

commit 13b3d80e80d22492da45eb0cf56e7846ed4e33a4

Zend_Auth_Adapter_Ldap and Zend_Ldap * added offline tests for when LDAP services are unavailable (e.g., by default in distribution) * added Zend_Auth_Adapter_Ldap::getUsername() and getPassword() * allow instantiation of Zend_Ldap despite absence of ldap extension * changed some default test configuration constants (incomplete) * use array type hints (incomplete) * always use braces {..} for statements (e.g., if...else, foreach) (incomplete) * use @todo phpdoc tag * s/\t/    /g * relates to ZF-994 and ZF-2445
git-svn-id: http://framework.zend.com/svn/framework@7554 44c647ce-9c0f-0410-b52a-842ac1e357ba

trunk/incubator/library/Zend/Auth/Adapter/Ldap.php
@@ -24,6 +24,7 @@
  * @see Zend_Auth_Adapter_Interface
  */
 require_once 'Zend/Auth/Adapter/Interface.php';
+
 /**
  * @see Zend_Log
  */
@@ -72,41 +73,61 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
     protected $_logger = null;
 
     /**
-     * @param array $options An array of arrays of Zend_Ldap options
-     * @param string $username The username of the account being authenticated
-     * @param string $password The password of the account being authenticated
-     * @throws Zend_Auth_Adapter_Exception
+     * @param  array  $options  An array of arrays of Zend_Ldap options
+     * @param  string $username The username of the account being authenticated
+     * @param  string $password The password of the account being authenticated
+     * @return void
      */
-    public function __construct($options, $username = null, $password = null)
+    public function __construct(array $options = array(), $username = null, $password = null)
     {
-        if (!is_array($options)) {
-            require_once 'Zend/Auth/Adapter/Exception.php';
-            throw new Zend_Auth_Adapter_Exception('First parameter must be options array');
-        }
-        if ($username !== null)
-            $this->setUsername($username);
-        if ($password !== null)
-            $this->setPassword($password);
         $this->_options = $options;
+        if ($username !== null) {
+            $this->setUsername($username);
+        }
+        if ($password !== null) {
+            $this->setPassword($password);
+        }
     }
 
     /**
-     * @param string $username The username for binding
+     * Returns the username of the account being authenticated, or
+     * NULL if none is set.
+     *
+     * @return string|null
+     */
+    public function getUsername()
+    {
+        return $this->_username;
+    }
+
+    /**
+     * @param  string $username The username for binding
      * @return Zend_Auth_Adapter_Ldap Provides a fluent interface
      */
     public function setUsername($username)
     {
-        $this->_username = $username;
+        $this->_username = (string) $username;
         return $this;
     }
 
     /**
-     * @param string $password The password of the account being authenticated
+     * Returns the password of the account being authenticated, or
+     * NULL if none is set.
+     *
+     * @return string|null
+     */
+    public function getPassword()
+    {
+        return $this->_password;
+    }
+
+    /**
+     * @param  string $password The password of the account being authenticated
      * @return Zend_Auth_Adapter_Ldap Provides a fluent interface
      */
     public function setPassword($password)
     {
-        $this->_password = $password;
+        $this->_password = (string) $password;
         return $this;
     }
 
@@ -116,6 +137,9 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
     public function getLdap()
     {
         if ($this->_ldap === null) {
+            /**
+             * @see Zend_Ldap
+             */
             require_once 'Zend/Ldap.php';
             $this->_ldap = new Zend_Ldap();
         }

commit d67eecabbbb5ae02d014aab956feb7ff9ba61e86

* ZF-2572: Factored out Zend_Log; debug messages now returned in Zend_Auth_Result * setOptions now called outside of try/catch to alert operator of config err * Replaced Zend_Ldap options members with single options array * ZF-2570: Added type hints wherever possible * ZF-2474: Added explodeDn() to more accurately detect DN strings (fixes semicolon bug) * Replaced permittedOptions logic with array_diff_key (which was broken in r7554 anyway) * Updated TestConfiguration.php.dist with example configs * Tweeked and added some tests (e.g. testExplodeDnOperation)
git-svn-id: http://framework.zend.com/svn/framework@7862 44c647ce-9c0f-0410-b52a-842ac1e357ba

trunk/incubator/library/Zend/Auth/Adapter/Ldap.php
@@ -26,11 +26,6 @@
 require_once 'Zend/Auth/Adapter/Interface.php';
 
 /**
- * @see Zend_Log
- */
-require_once 'Zend/Log.php';
-
-/**
  * @category   Zend
  * @package    Zend_Auth
  * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
@@ -66,11 +61,6 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
      * @var string
      */
     protected $_password = null;
-
-    /**
-     * @var Zend_Log
-     */
-    protected $_logger = null;
 
     /**
      * @param  array  $options  An array of arrays of Zend_Ldap options
@@ -154,12 +144,16 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
     {
         require_once 'Zend/Ldap/Exception.php';
 
+        $messages = array();
+        $messages[0] = ''; // reserved
+        $messages[1] = ''; // reserved
+
         $username = $this->_username;
         $password = $this->_password;
 
         if (!$username) {
             $code = Zend_Auth_Result::FAILURE_IDENTITY_NOT_FOUND;
-            $messages = array('A username is required');
+            $messages[0] = 'A username is required';
             return new Zend_Auth_Result($code, '', $messages);
         }
         if (!$password) {
@@ -167,15 +161,14 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
              * treat an empty password as an anonymous bind.
              */
             $code = Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID;
-            $messages = array('A password is required');
+            $messages[0] = 'A password is required';
             return new Zend_Auth_Result($code, '', $messages);
         }
 
         $ldap = $this->getLdap();
 
         $code = Zend_Auth_Result::FAILURE;
-        $messages = array("Authority not found: $username");
-        $log = $this->getLogger();
+        $messages[0] = "Authority not found: $username";
 
         /* Iterate through each server and try to authenticate the supplied
          * credentials against it.
@@ -186,27 +179,28 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
                 require_once 'Zend/Auth/Adapter/Exception.php';
                 throw new Zend_Auth_Adapter_Exception('Adapter options array not in array');
             }
+            $ldap->setOptions($options);
 
             try {
-                $ldap->setOptions($options);
-                if ($log)
-                    $this->_logOptions($options);
 
                 $canonicalName = $ldap->getCanonicalAccountName($username);
 
-                if (isset($messages[1]))
-                    $this->_log($messages[1]);
-                $messages = array();
+                if ($messages[1])
+                    $messages[] = $messages[1];
+                $messages[1] = '';
+                $messages[] = $this->_optionsToString($options);
 
                 $ldap->bind($canonicalName, $password);
 
-                $this->_log("$canonicalName authentication successful", Zend_Log::INFO);
+                $messages[0] = '';
+                $messages[1] = '';
+                $messages[] = "$canonicalName authentication successful";
 
-                return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS, $canonicalName);
+                return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS, $canonicalName, $messages);
             } catch (Zend_Ldap_Exception $zle) {
 
                 /* LDAP based authentication is notoriously difficult to diagnose. Therefore
-                 * we bend over backwards to capture and log every possible bit of
+                 * we bend over backwards to capture and record every possible bit of
                  * information when something goes wrong.
                  */
 
@@ -221,63 +215,27 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
                     continue;
                 } else if ($err == Zend_Ldap_Exception::LDAP_NO_SUCH_OBJECT) {
                     $code = Zend_Auth_Result::FAILURE_IDENTITY_NOT_FOUND;
-                    $messages[] = "Account not found: $username";
+                    $messages[0] = "Account not found: $username";
                 } else if ($err == Zend_Ldap_Exception::LDAP_INVALID_CREDENTIALS) {
                     $code = Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID;
-                    $messages[] = 'Invalid credentials';
+                    $messages[0] = 'Invalid credentials';
                 } else {
-                    if ($log) {
-                        $str = $zle->getFile() . '(' . $zle->getLine() . '): ' . $zle->getMessage();
-                        $this->_log($str, Zend_Log::ERR);
-                        $str = str_replace($password, '*****', $zle->getTraceAsString());
-                        $this->_log($str, Zend_Log::WARN);
-                    }
-                    $messages[] = 'An unexpected failure occurred';
+                    $line = $zle->getLine();
+                    $messages[] = $zle->getFile() . "($line): " . $zle->getMessage();
+                    $messages[] = str_replace($password, '*****', $zle->getTraceAsString());
+                    $messages[0] = 'An unexpected failure occurred';
                 }
-                $messages[] = $zle->getMessage();
+                $messages[1] = $zle->getMessage();
             }
         }
 
         $msg = isset($messages[1]) ? $messages[1] : $messages[0];
-        $this->_log("$username authentication failed: $msg", Zend_Log::NOTICE);
+        $messages[] = "$username authentication failed: $msg";
 
         return new Zend_Auth_Result($code, $username, $messages);
     }
 
-    /**
-     * @param Zend_Log $logger The Zend_Log object that this adapter should use to log debugging information
-     * @return Zend_Auth_Adapter_Ldap Provides a fluent interface
-     */
-    public function setLogger($logger)
-    {
-        $this->_logger = $logger;
-        return $this;
-    }
-
-    /**
-     * @return Zend_Log The Zend_Log object used by this adapter to log debugging information
-     */
-    public function getLogger()
-    {
-        return $this->_logger;
-    }
-
-    /**
-     * @param $str The message to be logged
-     * @param int The Zend_Log priority value
-     * @return Zend_Auth_Adapter_Ldap Provides a fluent interface
-     */
-    protected function _log($str, $priority = Zend_Log::DEBUG)
-    {
-        $logger = $this->getLogger();
-        if ($logger) {
-            $str = 'Ldap: ' . str_replace("\n", "\n  ", $str);
-            $logger->log($str, $priority);
-        }
-        return $this;
-    }
-
-    private function _logOptions($options)
+    private function _optionsToString(array $options)
     {
         $str = '';
         foreach ($options as $key => $val) {
@@ -287,6 +245,6 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
         }
         if ($this->_password)
             $str = str_replace($this->_password, '*****', $str);
-        $this->_log($str);
+        return $str;
     }
 }

trunk/incubator/library/Zend/Ldap.php
@@ -49,20 +49,8 @@ class Zend_Ldap
      */
     protected $_resource = null;
 
-    protected $_host;
-    protected $_port;
-    protected $_useSsl;
-    protected $_username;
-    protected $_password;
-    protected $_bindRequiresDn;
-    protected $_baseDn;
-    protected $_accountCanonicalForm;
-    protected $_accountDomainName;
-    protected $_accountDomainNameShort;
-    protected $_accountFilterFormat;
-
     /**
-     * @param  string The string to escape.
+     * @param  string $str The string to escape.
      * @return string The escaped string
      */
     public static function filterEscape($str)
@@ -78,6 +66,62 @@ class Zend_Ldap
             $ret .= $ch;
         }
         return $ret;
+    }
+
+    /**
+     * @param  string $dn   The DN to parse
+     * @param  array  $keys An optional array to receive DN keys (e.g. CN, OU, DC, ...)
+     * @param  array  $vals An optional array to receive DN values
+     * @return bool   True if the DN was successfully parsed or false if the string is not a valid DN.
+     */
+    public static function explodeDn($dn, array &$keys = null, array &$vals = null)
+    {
+        /* This is a classic state machine parser. Each iteration of the
+         * loop processes one character. State 1 collects the key. When equals (=)
+         * is encountered the state changes to 2 where the value is collected
+         * until a comma (,) or semicolon (;) is encountered after which we switch back
+         * to state 1. If a backslash (\) is encountered, state 3 is used to collect the
+         * following character without engaging the logic of other states.
+         */
+        $key = null;
+        $slen = strlen($dn);
+        $state = 1;
+        $ko = $vo = 0;
+        for ($di = 0; $di <= $slen; $di++) {
+            $ch = $di == $slen ? 0 : $dn[$di];
+            switch ($state) {
+                case 1: // collect key
+                    if ($ch === '=') {
+                        $key = trim(substr($dn, $ko, $di - $ko));
+                        if ($keys !== null) {
+                            $keys[] = $key; 
+                        }
+                        $state = 2;
+                        $vo = $di + 1;
+                    } else if ($ch === ',' || $ch === ';') {
+                        return false;
+                    }
+                    break;
+                case 2: // collect value
+                    if ($ch === '\\') {
+                        $state = 3;
+                    } else if ($ch === ',' || $ch === ';' || $ch === 0) {
+                        if ($vals !== null) {
+                            $vals[] = trim(substr($dn, $vo, $di - $vo));
+                        }
+                        $state = 1;
+                        $ko = $di + 1;
+                    } else if ($ch === '=') {
+                        return false;
+                    }
+                    break;
+                case 3: // escaped
+                    $state = 2;
+                    break;
+            }
+        }
+
+        return $state === 1 && strtoupper($key) === 'DC'; 
     }
 
     /**
@@ -125,33 +169,30 @@ class Zend_Ldap
             'accountFilterFormat'       => null,
         );
 
-        foreach ($options as $optionKey => $optionValue) {
-            if (!array_key_exists($optionKey, $permittedOptions)) {
-                /**
-                 * @see Zend_Ldap_Exception
-                 */
-                require_once 'Zend/Ldap/Exception.php';
-                throw new Zend_Ldap_Exception(null, "Unknown Zend_Ldap option: $optionKey");
-            }
-            $this->{"_$optionKey"} = $optionValue;
+        $diff = array_diff_key($options, $permittedOptions);
+        if ($diff) {
+            list($key, $val) = each($diff);
+            require_once 'Zend/Ldap/Exception.php';
+            throw new Zend_Ldap_Exception(null, "Unknown Zend_Ldap option: $key");
         }
 
-        /* Account names should always be qualified with a domain. In some scenarios
-         * using non-qualified account names can lead to security vulnerbilities. If
-         * no account canonical form is specified, we guess based in what domain
-         * names have been supplied.
-         */
-        if (!$this->_accountCanonicalForm) {
-            if ($this->_accountDomainNameShort) {
-                $this->_accountCanonicalForm = Zend_Ldap::ACCTNAME_FORM_BACKSLASH;
-            } else if ($this->_accountDomainName) {
-                $this->_accountCanonicalForm = Zend_Ldap::ACCTNAME_FORM_PRINCIPAL;
-            } else {
-                $this->_accountCanonicalForm = Zend_Ldap::ACCTNAME_FORM_USERNAME;
+        foreach ($permittedOptions as $key => $val) {
+            if (!array_key_exists($key, $options)) {
+                $options[$key] = null;
             }
         }
+
+        $this->_options = $options;
 
         return $this;
+    }
+
+    /**
+     * @return array The current options.
+     */
+    public function getOptions()
+    {
+        return $this->_options;
     }
 
     /**
@@ -170,7 +211,7 @@ class Zend_Ldap
      */
     protected function _getHost()
     {
-        return $this->_host;
+        return $this->_options['host'];
     }
 
     /**
@@ -178,8 +219,8 @@ class Zend_Ldap
      */
     protected function _getPort()
     {
-        if ($this->_port)
-            return $this->_port;
+        if ($this->_options['port'])
+            return $this->_options['port'];
         return 0;
     }
 
@@ -188,7 +229,7 @@ class Zend_Ldap
      */
     protected function _getUsername()
     {
-        return $this->_username;
+        return $this->_options['username'];
     }
 
     /**
@@ -196,7 +237,7 @@ class Zend_Ldap
      */
     protected function _getPassword()
     {
-        return $this->_password;
+        return $this->_options['password'];
     }
 
     /**
@@ -204,7 +245,7 @@ class Zend_Ldap
      */
     protected function _getUseSsl()
     {
-        return $this->_useSsl;
+        return $this->_options['useSsl'];
     }
 
     /**
@@ -212,15 +253,43 @@ class Zend_Ldap
      */
     protected function _getBaseDn()
     {
-        return $this->_baseDn;
+        return $this->_options['baseDn'];
     }
+
+    /**
+     * @return string Either ACCTNAME_FORM_BACKSLASH, ACCTNAME_FORM_PRINCIPAL or ACCTNAME_FORM_USERNAME indicating the form usernames should be canonicalized to.
+     */
+    protected function _getAccountCanonicalForm()
+    {
+        /* Account names should always be qualified with a domain. In some scenarios
+         * using non-qualified account names can lead to security vulnerabilities. If
+         * no account canonical form is specified, we guess based in what domain
+         * names have been supplied.
+         */
+
+        $accountCanonicalForm = $this->_options['accountCanonicalForm'];
+        if (!$accountCanonicalForm) {
+            $accountDomainName = $this->_options['accountDomainName'];
+            $accountDomainNameShort = $this->_options['accountDomainNameShort'];
+            if ($accountDomainNameShort) {
+                $accountCanonicalForm = Zend_Ldap::ACCTNAME_FORM_BACKSLASH;
+            } else if ($accountDomainName) {
+                $accountCanonicalForm = Zend_Ldap::ACCTNAME_FORM_PRINCIPAL;
+            } else {
+                $accountCanonicalForm = Zend_Ldap::ACCTNAME_FORM_USERNAME;
+            }
+        }
+
+        return $accountCanonicalForm;
+    }
+
 
     /**
      * @return string A format string for building an LDAP search filter to match an account
      */
     protected function _getAccountFilterFormat()
     {
-        return $this->_accountFilterFormat;
+        return $this->_options['accountFilterFormat'];
     }
 
     /**
@@ -233,7 +302,7 @@ class Zend_Ldap
         $aname = Zend_Ldap::filterEscape($aname);
         if ($accountFilterFormat)
             return sprintf($accountFilterFormat, $aname);
-        if (!$this->_bindRequiresDn) {
+        if (!$this->_options['bindRequiresDn']) {
             // is there a better way to detect this?
             return "(&(objectClass=user)(sAMAccountName=$aname))";
         }
@@ -270,7 +339,7 @@ class Zend_Ldap
      */
     protected function _getAccountDn($acctname)
     {
-        if ($this->_isDnString($acctname))
+        if (Zend_Ldap::explodeDn($acctname))
             return $acctname;
         $acctname = $this->getCanonicalAccountName($acctname, Zend_Ldap::ACCTNAME_FORM_USERNAME);
         $acct = $this->_getAccount($acctname, array('dn'));
@@ -285,11 +354,13 @@ class Zend_Ldap
     {
         if ($dname === null)
             return true;
-        if ($this->_accountDomainName === null && $this->_accountDomainNameShort === null)
+        $accountDomainName = $this->_options['accountDomainName'];
+        $accountDomainNameShort = $this->_options['accountDomainNameShort'];
+        if ($accountDomainName === null && $accountDomainNameShort === null)
             return true;
-        if (strcasecmp($dname, $this->_accountDomainName) == 0)
+        if (strcasecmp($dname, $accountDomainName) == 0)
             return true;
-        if (strcasecmp($dname, $this->_accountDomainNameShort) == 0)
+        if (strcasecmp($dname, $accountDomainNameShort) == 0)
             return true;
         return false;
     }
@@ -328,29 +399,31 @@ class Zend_Ldap
         $uname = strtolower($uname);
 
         if ($form === 0)
-            $form = $this->_accountCanonicalForm;
+            $form = $this->_getAccountCanonicalForm();
 
         switch ($form) {
             case Zend_Ldap::ACCTNAME_FORM_USERNAME:
                 return $uname;
             case Zend_Ldap::ACCTNAME_FORM_BACKSLASH:
-                if (!$this->_accountDomainNameShort) {
+                $accountDomainNameShort = $this->_options['accountDomainNameShort'];
+                if (!$accountDomainNameShort) {
                     /**
                      * @see Zend_Ldap_Exception
                      */
                     require_once 'Zend/Ldap/Exception.php';
                     throw new Zend_Ldap_Exception(null, 'Option required: accountDomainNameShort');
                 }
-                return "$this->_accountDomainNameShort\\$uname";
+                return "$accountDomainNameShort\\$uname";
             case Zend_Ldap::ACCTNAME_FORM_PRINCIPAL:
-                if (!$this->_accountDomainName) {
+                $accountDomainName = $this->_options['accountDomainName'];
+                if (!$accountDomainName) {
                     /**
                      * @see Zend_Ldap_Exception
                      */
                     require_once 'Zend/Ldap/Exception.php';
                     throw new Zend_Ldap_Exception(null, 'Option required: accountDomainName');
                 }
-                return "$uname@$this->_accountDomainName";
+                return "$uname@$accountDomainName";
             default:
                 /**
                  * @see Zend_Ldap_Exception
@@ -365,7 +438,7 @@ class Zend_Ldap
      * @return array An array of the attributes representing the account
      * @throws Zend_Ldap_Exception
      */
-    private function _getAccount($acctname, $attrs = null)
+    private function _getAccount($acctname, array $attrs = null)
     {
         $baseDn = $this->_getBaseDn();
         if (!$baseDn) {
@@ -542,11 +615,6 @@ class Zend_Ldap
         throw new Zend_Ldap_Exception("Failed to connect to LDAP server: $host:$port");
     }
 
-    protected function _isDnString($str)
-    {
-        return $str && stristr($str, ',DC=');
-    }
-
     /**
      * @param string $username The username for authenticating the bind
      * @param string $password The password for authenticating the bind
@@ -573,8 +641,8 @@ class Zend_Ldap
 
         /* Check to make sure the username is in DN form.
          */
-        if (!$this->_isDnString($username)) {
-            if ($this->_bindRequiresDn) {
+        if (!Zend_Ldap::explodeDn($username)) {
+            if ($this->_options['bindRequiresDn']) {
                 /* moreCreds stops an infinite loop if _getUsername does not
                  * return a DN and the bind requires it
                  */

trunk/incubator/tests/TestConfiguration.php.dist
@@ -83,7 +83,32 @@ define('TESTS_ZEND_LDAP_ONLINE_ENABLED', false);
 
 /* These largely map to the options described in the Zend_Ldap and
  * Zend_Auth_Adapter_Ldap documentation.
- * [pls change these values in SVN to match the primary test env if necessary]
+ *
+ * Example Configuration for Active Directory:
+ *                      HOST: dc1.w.net
+ *                   USE_SSL: true
+ *                  USERNAME: CN=User 1,CN=Users,DC=w,DC=net
+ *            PRINCIPAL_NAME: user1@w.net
+ *             LDAP_PASSWORD: pass1
+ *                   BASE_DN: CN=Users,DC=w,DC=net
+ *               DOMAIN_NAME: w.net
+ * ACCOUNT_DOMAIN_NAME_SHORT: W
+ *              ALT_USERNAME: user2
+ *                    ALT_DN: CN=User 2,CN=Users,DC=w,DC=net
+ *              ALT_PASSWORD: pass2
+ *
+ * Example Configuration for OpenLDAP
+ *                      HOST: s0.foo.net
+ *                  USERNAME: CN=user1,DC=foo,DC=net
+ *            PRINCIPAL_NAME: user1@foo.net
+ *             LDAP_PASSWORD: pass1
+ *          BIND_REQUIRES_DN: true
+ *                   BASE_DN: OU=Sales,DC=w,DC=net
+ *               DOMAIN_NAME: foo.net
+ * ACCOUNT_DOMAIN_NAME_SHORT: FOO
+ *              ALT_USERNAME: abaker
+ *                    ALT_DN: CN=Alice Baker,OU=Sales,DC=foo,DC=net
+ *              ALT_PASSWORD: apass
  */
 define('TESTS_ZEND_LDAP_HOST', 'localhost');
 //define('TESTS_ZEND_LDAP_PORT', 389);
@@ -96,9 +121,8 @@ define('TESTS_ZEND_LDAP_BASE_DN', 'OU=Sales,DC=example,DC=com');
 //define('TESTS_ZEND_LDAP_ACCOUNT_FILTER_FORMAT', '(&(objectClass=posixAccount)(uid=%s))');
 define('TESTS_ZEND_LDAP_ACCOUNT_DOMAIN_NAME', 'example.com');
 define('TESTS_ZEND_LDAP_ACCOUNT_DOMAIN_NAME_SHORT', 'EXAMPLE');
-/* Must be creds for account other than the default above
- */
 define('TESTS_ZEND_LDAP_ALT_USERNAME', 'anotherUser');
+define('TESTS_ZEND_LDAP_ALT_DN', 'CN=Another User,OU=Sales,DC=example,DC=com');
 define('TESTS_ZEND_LDAP_ALT_PASSWORD', null);
 
 /**

trunk/incubator/tests/Zend/Ldap/BindTest.php
@@ -159,7 +159,8 @@ class Zend_Ldap_BindTest extends PHPUnit_Framework_TestCase
         try {
             $ldap->bind($this->_altUsername, 'invalid');
         } catch (Zend_Ldap_Exception $zle) {
-            $this->assertContains('Invalid credentials', $zle->getMessage());
+			$message = str_replace("\n", " ", $zle->getMessage());
+            $this->assertContains('Invalid credentials', $message);
         }
     }
     public function testRequiresDnWithoutDnBind()

trunk/incubator/tests/Zend/Ldap/CanonTest.php
@@ -105,6 +105,11 @@ class Zend_Ldap_CanonTest extends PHPUnit_Framework_TestCase
             $this->assertTrue(strstr($msg, 'Invalid credentials') || strstr($msg, 'No such object'));
         }
     }
+    public function testDnCanon()
+    {
+		$ldap = new Zend_Ldap($this->_options);
+		$name = $ldap->getCanonicalAccountName(TESTS_ZEND_LDAP_ALT_DN, Zend_Ldap::ACCTNAME_FORM_DN);
+    }
     public function testBindCanon()
     {
         /**

trunk/incubator/tests/Zend/Ldap/OfflineTest.php
@@ -84,4 +84,34 @@ class Zend_Ldap_OfflineTest extends PHPUnit_Framework_TestCase
             $this->assertEquals("Unknown Zend_Ldap option: $optionName", $e->getMessage());
         }
     }
+
+    /**
+     * @return void
+     */
+    public function testExplodeDnOperation()
+    {
+		$inputs = array(
+			'CN=Alice Baker,CN=Users,DC=example,DC=com' => true,
+			'CN=Baker\\, Alice,CN=Users,DC=example,DC=com' => true,
+			'OU=Sales,DC=local' => true,
+			'OU=Sales;DC=local' => true,
+			'OU=Sales ,DC=local' => true,
+			'OU=Sales, dC=local' => true,
+			'ou=Sales , DC=local' => true,
+			'OU=Sales ; dc=local' => true,
+			'DC=local' => true,
+			' DC=local' => true,
+			'DC= local  ' => true,
+			'username' => false,
+			'username@example.com' => false,
+			'EXAMPLE\\username' => false,
+			'CN=,Alice Baker,CN=Users,DC=example,DC=com' => false,
+			'CN=Users,DC==example,DC=com' => false,
+		);
+
+		foreach ($inputs as $dn => $expected) {
+			$ret = Zend_Ldap::explodeDn($dn);
+			$this->assertTrue($ret === $expected);
+		}
+	}
 }