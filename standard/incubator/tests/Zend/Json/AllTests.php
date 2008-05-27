99f4ce04087fe2d38ca521c69d54ff15576db4fd
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-05-27 18:44:35 +0000

    r19914@zlaptop:  matthew | 2008-05-27 14:51:05 -0400   * Move JSON-RPC server code to incubator
    git-svn-id: http://framework.zend.com/svn/framework@9555 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- standard/incubator/tests/Zend/Json/AllTests.php
+++ standard/incubator/tests/Zend/Json/AllTests.php
@@ -1,12 +1,16 @@
 <?php
 if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'Zend_XmlRpc_AllTests::main');
+    define('PHPUnit_MAIN_METHOD', 'Zend_Json_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
+require_once dirname(__FILE__) . '/../../TestHelper.php';
 
 require_once 'Zend/Json/ServerTest.php';
+require_once 'Zend/Json/Server/ErrorTest.php';
+require_once 'Zend/Json/Server/RequestTest.php';
+require_once 'Zend/Json/Server/ResponseTest.php';
+require_once 'Zend/Json/Server/SmdTest.php';
+require_once 'Zend/Json/Server/Smd/ServiceTest.php';
 
 class Zend_Json_AllTests
 {
@@ -20,6 +24,11 @@ class Zend_Json_AllTests
         $suite = new PHPUnit_Framework_TestSuite('Zend Framework - Zend_Json');
 
         $suite->addTestSuite('Zend_Json_ServerTest');
+        $suite->addTestSuite('Zend_Json_Server_ErrorTest');
+        $suite->addTestSuite('Zend_Json_Server_RequestTest');
+        $suite->addTestSuite('Zend_Json_Server_ResponseTest');
+        $suite->addTestSuite('Zend_Json_Server_SmdTest');
+        $suite->addTestSuite('Zend_Json_Server_Smd_ServiceTest');
        
         return $suite;
     }
