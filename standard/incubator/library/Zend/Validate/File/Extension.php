891086f97e8730fb4503dfa351b6c20bb4ca4e0f
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-06-26 20:50:20 +0000

    Zend_File_Transfer (Zend_Validate):  - initial validators for extension and size added
    git-svn-id: http://framework.zend.com/svn/framework@9794 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ standard/incubator/library/Zend/Validate/File/Extension.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category  Zend
+ * @package   Zend_Validate
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ * @version   $Id: $
+ */
+
+/**
+ * @see Zend_Validate_Abstract
+ */
+require_once 'Zend/Validate/Abstract.php';
+
+/**
+ * Validator for the maximum size of a file up to a max of 2GB
+ *
+ * @category  Zend
+ * @package   Zend_Validate
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Validate_File_Extension extends Zend_Validate_Abstract
+{
+    const FALSE_EXTENSION = 'fileExtensionFalse';
+    const NOT_READABLE    = 'fileNotReadable';
+
+    /**
+     * @var array
+     */
+    protected $_messageTemplates = array(
+        self::FALSE_EXTENSION => "The file '%value%' has a false extension",
+        self::NOT_READABLE    => "The file '%value%' can not be read"
+    );
+
+    /**
+     * @var array
+     */
+    protected $_messageVariables = array(
+        'extension' => '_extension'
+    );
+
+    /**
+     * Sets validator options
+     *
+     * @param  string|array $extension
+     * @return void
+     */
+    public function __construct($extension)
+    {
+        $this->setExtension($extension);
+    }
+
+    /**
+     * Returns the set file extension
+     *
+     * @return string
+     */
+    public function getExtension()
+    {
+        $extension = implode(',', $this->_extension);
+        return $extension;
+    }
+
+    /**
+     * Sets the file extensions
+     *
+     * @param  string|array $extension The extensions to validate
+     * @return Zend_Validate_File_Size Provides a fluent interface
+     */
+    public function setExtension($extension)
+    {
+        if (is_string($extension) === true) {
+            $extension = explode(',', $extension);
+        }
+
+        $this->_extension = $extension;
+        return $this;
+    }
+
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if and only if the fileextension of $value is included in the
+     * set extension list
+     *
+     * @param  string $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        // Is file readable ?
+        if (@is_readable($value) === false) {
+            $this->_error(self::NOT_READABLE);
+            return false;
+        }
+
+        $info = @pathinfo($value);
+        if (in_array($info['extension'], $this->_extension) === true) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ standard/incubator/library/Zend/Validate/File/Size.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category  Zend
+ * @package   Zend_Validate
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ * @version   $Id: $
+ */
+
+/**
+ * @see Zend_Validate_Abstract
+ */
+require_once 'Zend/Validate/Abstract.php';
+
+/**
+ * Validator for the maximum size of a file up to a max of 2GB
+ *
+ * @category  Zend
+ * @package   Zend_Validate
+ * @copyright Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class Zend_Validate_File_Size extends Zend_Validate_Abstract
+{
+    const TOO_BIG      = 'fileSizeTooBig';
+    const TOO_SMALL    = 'fileSizeTooSmall';
+    const NOT_READABLE = 'fileNotReadable';
+    
+    /**
+     * @var array
+     */
+    protected $_messageTemplates = array(
+        self::TOO_BIG      => "The file '%value%' is bigger than allowed",
+        self::TOO_SMALL    => "The file '%value%' is smaller than allowed",
+        self::NOT_READABLE => "The file '%value%' can not be read"
+    );
+
+    /**
+     * @var array
+     */
+    protected $_messageVariables = array(
+        'min' => '_min',
+        'max' => '_max'
+    );
+
+    /**
+     * Minimum filesize
+     *
+     * @var integer
+     */
+    protected $_min;
+
+    /**
+     * Maximum filesize
+     *
+     * If null, there is no maximum file size
+     *
+     * @var integer|null
+     */
+    protected $_max;
+
+    /**
+     * Sets validator options
+     *
+     * @param  integer $min
+     * @param  integer $max
+     * @return void
+     */
+    public function __construct($min = 0, $max = null)
+    {
+        $this->setMin($min);
+        $this->setMax($max);
+    }
+
+    /**
+     * Returns the minimum filesize
+     *
+     * @return integer
+     */
+    public function getMin()
+    {
+        $min = $this->_toByteString($this->_min);
+        return $min;
+    }
+
+    /**
+     * Sets the minimum filesize
+     *
+     * @param  integer $min            The minimum filesize
+     * @throws Zend_Validate_Exception When min is greater than max
+     * @return Zend_Validate_File_Size Provides a fluent interface
+     */
+    public function setMin($min)
+    {
+        $min = $this->_fromByteString($min);
+        if (($this->_max !== null) and ($min > $this->_max)) {
+            require_once 'Zend/Validate/Exception.php';
+            throw new Zend_Validate_Exception("The minimum must be less than or equal to the maximum filesize, but $min >"
+                                            . " $this->_max");
+        }
+        $this->_min = max(0, (integer) $min);
+        return $this;
+    }
+
+    /**
+     * Returns the maximum filesize
+     *
+     * @return integer|null
+     */
+    public function getMax()
+    {
+        $max = $this->_toByteString($this->_max);
+        return $max;
+    }
+
+    /**
+     * Sets the maximum filesize
+     *
+     * @param  integer|null $max       The maximum filesize
+     * @throws Zend_Validate_Exception When max is smaller than min
+     * @return Zend_Validate_StringLength Provides a fluent interface
+     */
+    public function setMax($max)
+    {
+        $max = $this->_fromByteString($max);
+        if (null === $max) {
+            $this->_max = null;
+        } else if ($max < $this->_min) {
+            require_once 'Zend/Validate/Exception.php';
+            throw new Zend_Validate_Exception("The maximum must be greater than or equal to the minimum length, but "
+                                            . "$max < $this->_min");
+        } else {
+            $this->_max = (integer) $max;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if and only if the filesize of $value is at least min and
+     * not bigger than max (when max is not null).
+     *
+     * @param  string $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        // Is file readable ?
+        if (@is_readable($value) === false) {
+            $this->_error(self::NOT_READABLE);
+            return false;
+        }
+
+        // limited to 2GB files
+        $size = @filesize($value);
+        $this->_setValue($size);
+
+        if ($size < $this->_min) {
+            $this->_error(self::TOO_SMALL);
+        }
+
+        if (($this->_max !== null) and ($this->_max < $size)) {
+            $this->_error(self::TOO_BIG);
+        }
+
+        if (count($this->_messages) > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Returns the formatted size
+     *
+     * @param  integer $size
+     * @return string
+     */
+    private function _toByteString($size) {
+        $sizes = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        for ($i=0; $size > 1024 && $i < 9; $i++) {
+            $size /= 1024;
+        }
+
+        return round($size, 2).$sizes[$i];
+    }
+
+    /**
+     * Returns the unformatted size
+     *
+     * @param  string $size
+     * @return integer
+     */
+    private function _fromByteString($size) {
+        $type  = substr($size, -2);
+        $value = substr($size, 0, -2);
+        switch ($type) {
+            case 'YB':
+                $value *= (1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024);
+                break;
+
+            case 'ZB':
+                $value *= (1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024);
+                break;
+            case 'EB':
+                $value *= (1024 * 1024 * 1024 * 1024 * 1024 * 1024);
+                break;
+
+            case 'PB':
+                $value *= (1024 * 1024 * 1024 * 1024 * 1024);
+                break;
+
+            case 'TB':
+                $value *= (1024 * 1024 * 1024 * 1024);
+                break;
+
+            case 'GB':
+                $value *= (1024 * 1024 * 1024);
+                break;
+
+            case 'MB':
+                $value *= (1024 * 1024);
+                break;
+
+            case 'kB':
+                $value *= 1024;
+                break;
+
+            default:
+                break;
+        }
+
+        return $value;
+    }
+}
