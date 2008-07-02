4caa859547b59925b227a8c314c45c00e389ecca
norm2782 <norm2782@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-07-02 07:22:54 +0000

    Rename Zym  to Zend.
    git-svn-id: http://framework.zend.com/svn/framework@9869 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- standard/incubator/library/Zend/View/Helper/Html/Abstract.php
+++ standard/incubator/library/Zend/View/Helper/Html/Abstract.php
@@ -27,7 +27,7 @@
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-abstract class Zym_View_Helper_Html_Abstract
+abstract class Zend_View_Helper_Html_Abstract
 {
     /**
      * Newline
