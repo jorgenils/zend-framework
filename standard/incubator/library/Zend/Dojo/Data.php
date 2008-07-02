978e6c72b8555ecadb7b4604bc854942034539ca
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-07-02 19:21:51 +0000

    r20338@zlaptop:  matthew | 2008-07-02 15:21:07 -0400   * Document Zend_Dojo_Data (ZF-3552)   * Make identifier first argument to constructor; cannot add items without an     identifier.
    git-svn-id: http://framework.zend.com/svn/framework@9897 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- standard/incubator/library/Zend/Dojo/Data.php
+++ standard/incubator/library/Zend/Dojo/Data.php
@@ -52,18 +52,18 @@ class Zend_Dojo_Data implements ArrayAccess,Iterator,Countable
     /**
      * Constructor
      * 
-     * @param  array|Traversable|null $items 
      * @param  string|null $identifier 
+     * @param  array|Traversable|null $items 
      * @param  string|null $label 
      * @return void
      */
-    public function __construct($items = null, $identifier = null, $label = null) 
+    public function __construct($identifier = null, $items = null, $label = null) 
     { 
-        if (null !== $items) { 
-            $this->setItems($items); 
-        } 
         if (null !== $identifier) { 
             $this->setIdentifier($identifier); 
+        } 
+        if (null !== $items) { 
+            $this->setItems($items); 
         } 
         if (null !== $label) { 
             $this->setLabel($label); 
