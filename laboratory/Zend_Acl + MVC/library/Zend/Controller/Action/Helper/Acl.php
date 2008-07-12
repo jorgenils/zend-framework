c718d6534734e3a931a39f2231a22718159ee54d
aldemar <aldemar@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2008-07-12 03:37:43 +0000

    git-svn-id: http://framework.zend.com/svn/framework@10055 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- laboratory/Zend_Acl + MVC/library/Zend/Controller/Action/Helper/Acl.php
+++ laboratory/Zend_Acl + MVC/library/Zend/Controller/Action/Helper/Acl.php
@@ -17,7 +17,7 @@
  * @subpackage Zend_Controller_Action_Helper
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id: AjaxContext.php 9098 2008-03-30 19:29:10Z thomas $
+ * @version    $Id$
  */
 
 /** Zend_Controller_Action_Helper_Abstract */
